da8ee147081dc4765ba6efba987f636008b6c741
Nedas Jankauskas <nedasj44@gmail.com>
2024-10-08 22:54:08 +0300

    db.js change

--- db.js
+++ db.js
@@ -4,12 +4,12 @@ const { Pool } = require('pg');
 
 // Create a new pool for managing connections
 const pool = new Pool({
-    user: 'postgres',        // PostgreSQL username
-    host: 'crud.postgres.database.azure.com',            // PostgreSQL server address
-    database: 'postgres',    // Your database name
-    password: 'Universitetas.456',    // PostgreSQL password
-    port: 5432,                   // PostgreSQL port (default is 5432)
-    ssl: { rejectUnauthorized: false }  
+    user: process.env.PGUSER,         // PostgreSQL username
+    host: process.env.PGHOST,         // PostgreSQL server address
+    database: process.env.PGDATABASE, // Your database name
+    password: process.env.PGPASSWORD, // PostgreSQL password
+    port: process.env.PGPORT || 5432, // PostgreSQL port (default is 5432)
+    ssl: { rejectUnauthorized: false } // SSL setting for Azure PostgreSQL
 });
 
 // Export a query function that can be used to interact with the database
